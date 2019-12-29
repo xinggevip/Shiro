e27a8bc5ad9617718d81e64ce918f5b422b28b67
xinggevip <1511844263@qq.com>
2019-12-29 21:50:00 +0800

    Shiro授权之自定义realm授权

--- src/main/java/com/qiangssvip/App.java
+++ src/main/java/com/qiangssvip/App.java
@@ -109,10 +109,65 @@ public class App
 
 
     }
+    public static void test3(){
+        /**
+         * 1.构建securityManager工厂
+         * 2.通过工厂创建securityManager
+         * 3.将securityManager设置到运行环境中
+         * 4.创建一个Subject实例
+         * 5.创建token令牌
+         * 6.用户登录
+         * 7.用户退出
+         * */
+
+        /* 1.构建securityManager工厂  ini  加载配置 */
+        IniSecurityManagerFactory factory = new IniSecurityManagerFactory("classpath:shiro.ini");
+        /* 2.通过工厂创建securityManager */
+        SecurityManager securityManager = factory.getInstance();
+        /* 3.将securityManager设置到运行环境中 */
+        SecurityUtils.setSecurityManager(securityManager);
+        /* 4.创建一个Subject实例 */
+        Subject subject = SecurityUtils.getSubject();
+        /* 5.创建token令牌 */
+        UsernamePasswordToken token = new UsernamePasswordToken("itlike", "admin");
+        /* 6.用户登录 */
+
+        try {
+            subject.login(token);
+        }catch (UnknownAccountException e){
+            System.out.println("账号不存在");
+//            return;
+        }catch (IncorrectCredentialsException e){
+            System.out.println("密码错误");
+//            return;
+        }
+        System.out.println("是否认证成功" + subject.isAuthenticated());
+
+        /* 认证之后才取做授权 */
+        /* 判断当前用户是否有某一个角色和某一个权限 */
+        System.out.println("当前用户是否拥有role1角色：" + subject.hasRole("role1"));
+        /* 判断当前用户是否拥有多个角色 */
+        System.out.println("当前用户是否拥有role1和role2角色：" + subject.hasAllRoles(Arrays.asList("role1","role2")));
+        /* 判断当前用户是否拥有某一个权限 */
+        System.out.println("当前用户是否拥有user:create权限" + subject.isPermitted("user:create"));
+        /* 判断当前用户是否拥有多个权限 */
+        System.out.println("当前用户是否拥有user:create和user:update权限" + subject.isPermittedAll("user:create","user:update"));
+
+        /* 7.用户退出 */
+        subject.logout();
+        System.out.println("是否认证成功" + subject.isAuthenticated());
+
+
+
+
+
+
+    }
     public static void main( String[] args )
     {
         // test1();
-        test2();
+        // test2();
+        test3();
     }
 
 }
--- src/main/java/com/qiangssvip/Realm.java
+++ src/main/java/com/qiangssvip/Realm.java
@@ -5,9 +5,12 @@ import org.apache.shiro.authc.AuthenticationInfo;
 import org.apache.shiro.authc.AuthenticationToken;
 import org.apache.shiro.authc.SimpleAuthenticationInfo;
 import org.apache.shiro.authz.AuthorizationInfo;
+import org.apache.shiro.authz.SimpleAuthorizationInfo;
 import org.apache.shiro.realm.AuthorizingRealm;
 import org.apache.shiro.subject.PrincipalCollection;
 import org.apache.shiro.util.ByteSource;
+
+import java.util.ArrayList;
 
 public class Realm extends AuthorizingRealm {
     /* 认证 */
@@ -31,7 +34,25 @@ public class Realm extends AuthorizingRealm {
     /* 授权 */
     @Override
     protected AuthorizationInfo doGetAuthorizationInfo(PrincipalCollection principalCollection) {
-        return null;
+
+        /* 获取当前身份信息 */
+        Object primaryPrincipal = principalCollection.getPrimaryPrincipal();
+
+        /* 假设   用户的据角色  权限 */
+        ArrayList<String> roles = new ArrayList<>();
+        roles.add("role1");
+        roles.add("role2");
+
+        ArrayList<String> permissions = new ArrayList<>();
+        permissions.add("user:create");
+        permissions.add("user:update");
+
+        /* 把角色和权限添加到授权当中 */
+        SimpleAuthorizationInfo info = new SimpleAuthorizationInfo();
+        info.addRoles(roles);
+        info.addStringPermissions(permissions);
+
+        return info;
     }
 
 
