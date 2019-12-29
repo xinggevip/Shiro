9deec1bbfa47f886f90bfe163afaa0e461fa8107
xinggevip <1511844263@qq.com>
2019-12-29 20:57:47 +0800

    Shiro散列密码

--- src/test/java/com/qiangssvip/AppTest.java
+++ src/test/java/com/qiangssvip/AppTest.java
@@ -1,8 +1,10 @@
 package com.qiangssvip;
 
-import static org.junit.Assert.assertTrue;
-
+import org.apache.shiro.crypto.hash.Md5Hash;
+import org.apache.shiro.crypto.hash.SimpleHash;
 import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
 
 /**
  * Unit test for simple App.
@@ -16,5 +18,16 @@ public class AppTest
     public void shouldAnswerWithTrue()
     {
         assertTrue( true );
+    }
+
+    @Test
+    public void MdTest(){
+        /* 散列密码 同样的数据 同样的加密 同样的结果 */
+        Md5Hash md5Hash = new Md5Hash("admin", "jiayan", 3); // 1c417c8ac6596f5dd639f0924d760e43
+        System.out.println(md5Hash);
+
+        SimpleHash hash = new SimpleHash("md5", "admin", "jiayan", 3); // 1c417c8ac6596f5dd639f0924d760e43
+        System.out.println(hash);
+
     }
 }
